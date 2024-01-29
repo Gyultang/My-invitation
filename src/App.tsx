1e9e137c7fed0617d8b361186092de37d05f524a
Gyuri Kim <rnfl0318@naver.com>
2024-01-29 09:57:35 +0900

    error, loading 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
 import styles from './App.module.scss'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  const [card, setCard] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  // 데이터호출
+  // 최초에 랜더링이 한번한 후 데이터를 최초1번호출하기위해 useEffect에 데이터를 불러와줌
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8888/party')
+      .then((res) => {
+        if (res.ok === false) {
+          throw new Error('정보를 불러오지 못했습니다.')
+        }
+        return res.json()
+      })
+      .then((data) => {
+        setCard(data)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.log('에러발생', e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(card)}</div>
 }
 
 export default App
